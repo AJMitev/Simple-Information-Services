3047545d2d3927f9164c42e736a7bf9b0da7d561
AJMitev <ajmitev@gmail.com>
2019-06-02 17:56:55 +0300

    Implement ViewEngine and add tests

---
+++ SIS/SIS.MvcFramework.Tests/TestViewEngine.cs
@@ -0,0 +1,14 @@
+﻿namespace SIS.MvcFramework.Tests
+{
+    using Xunit;
+
+    public class TestViewEngine
+    {
+        [Theory]
+        public void TestGetHtml(string testFileName)
+        {
+            //TODO Implement this.
+            //https://youtu.be/F5kuK80hWUM?t=3656
+        }
+    }
+}
---
+++ SIS/SIS.MvcFramework/ViewEngine/SisViewEngine.cs
@@ -0,0 +1,10 @@
+﻿namespace SIS.MvcFramework.ViewEngine
+{
+    class SisViewEngine : IViewEngine
+    {
+        public string GetHtml<T>(string viewContent, T model)
+        {
+            return viewContent;
+        }
+    }
+}
